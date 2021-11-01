7b941eef12a497d31d4fc7eb214554124b4d7d97
noppefoxwolf <noppelabs@gmail.com>
2021-11-02 00:50:54 +0900

    Initial commit

---
+++ CollectionViewListCell.xctemplate/___FILEBASENAME___CollectionViewListCell.swift
@@ -0,0 +1,55 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+//
+
+import Foundation
+import SwiftRichString
+import UIKit
+
+struct ___VARIABLE_productName:identifier___ContentConfiguration: UIContentConfiguration {
+
+    func makeContentView() -> UIView & UIContentView {
+        ___VARIABLE_productName:identifier___ContentView(self)
+    }
+
+    func updated(for state: UIConfigurationState) -> ___VARIABLE_productName:identifier___ContentConfiguration {
+        self
+    }
+}
+
+class ___VARIABLE_productName:identifier___CollectionViewListCell: UICollectionViewListCell {
+
+}
+
+class ___VARIABLE_productName:identifier___ContentView: UIView, UIContentView {
+
+    var configuration: UIContentConfiguration {
+        didSet {
+            self.apply(configuration)
+        }
+    }
+
+    init(_ configuration: ___VARIABLE_productName:identifier___ContentConfiguration) {
+        self.configuration = configuration
+        super.init(frame: .null)
+
+        // setup views
+
+        apply(configuration)
+    }
+
+    func apply(_ configuration: UIContentConfiguration) {
+        guard let configuration = configuration as? ___VARIABLE_productName:identifier___ContentConfiguration else { return }
+        
+        // configure views
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CollectionViewListCellTemplate
+ 
